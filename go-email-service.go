ab770587902517b17c5c7e9826238b1e5875ee00
Kfir Stri <kfirstar@gmail.com>
2017-04-07 15:34:21 +0300

    added csv files for example and now the code reads these files

--- go-email-service.go
+++ go-email-service.go
@@ -1,40 +1,98 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"fmt"
-	"strings"
+	"io"
+	"os"
+	"strconv"
 
 	"github.com/kfirstri/go-email-service/models"
 )
 
-func main() {
-	fmt.Println("Hello!")
-	u := make(map[int]models.User)
+// Consts for all files and formats
+const usersFile = "users.csv"
+const groupsFile = "group_members.csv"
+const preferencesFile = "user_email_preferences.csv"
+const emailsFile = "emails.csv"
+const outputFolder = "sent_emails/"
+const emailSubjectFormat = "%v_%v_subject.txt"
+const emailBodyFormat = "%v_%v_body.html"
 
-	var us models.User
-
-	us.UserID = 1
-	us.Firstname = "Kfir"
-	us.Lastname = "Stri"
-	us.Email = "kfir@gmail.com"
-	us.Company = "world"
-
-	u[1] = us
-
-	fmt.Println(u[1])
-
-	str := "1,Michael,Gelfand,michael@senexx.com,Senexx"
-
-	r := csv.NewReader(strings.NewReader(str))
-
-	record, err := r.Read()
+// ReadFile reads the csv file specified in filename
+func ReadFile(filename string, handleRecord func([]string)) error {
+	// Open the file
+	file, err := os.Open(filename)
+	defer file.Close()
 
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
 
-	us, err = models.MakeUserFromRecord(record)
+	// Create a new CSV reader
+	csvReader := csv.NewReader(bufio.NewReader(file))
 
-	fmt.Println(us)
+	// Start reading
+	csvRecord, err := csvReader.Read()
+
+	// Loop until eof
+	for err != io.EOF {
+
+		// Make sure we don't some different error
+		if err != nil {
+			return err
+		}
+
+		handleRecord(csvRecord)
+		csvRecord, err = csvReader.Read()
+	}
+
+	return nil
+}
+
+func main() {
+	// Initialize users and groups
+	var Users = make(map[int]*models.User)
+	var Groups = make(map[int][]int)
+	var err error
+
+	// Create a new User for each record in usersFile
+	err = ReadFile(usersFile, func(record []string) {
+		user, err := models.NewUserFromRecord(record)
+
+		if err != nil {
+			fmt.Printf("Error in creating new User: %v", err)
+		}
+
+		// Add the new user to the Users map
+		Users[user.UserID] = user
+	})
+
+	if err != nil {
+		fmt.Printf("Error in reading users file: %v", err)
+	}
+
+	for userID, user := range Users {
+		fmt.Printf("User ID %v, User = %+v\n", userID, user)
+	}
+
+	// Fill the Groups map
+	err = ReadFile(groupsFile, func(record []string) {
+		groupID, _ := strconv.Atoi(record[0])
+		userID, _ := strconv.Atoi(record[1])
+
+		_, exists := Groups[groupID]
+
+		if exists {
+			Groups[groupID] = append(Groups[groupID], userID)
+		} else {
+			Groups[groupID] = make([]int, 1)
+			Groups[groupID][0] = userID
+		}
+	})
+
+	if err != nil {
+		fmt.Printf("Error in reading groups file: %v", err)
+	}
 }
